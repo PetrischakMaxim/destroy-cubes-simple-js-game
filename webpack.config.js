c4f183a0267445a59d43acc641a21a1578314dd6
Petrischak Maxim <petrischak.maxim@gmail.com>
2020-11-10 13:12:13 +0200

    Развернул проект и сделал базовый каркас игры

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,16 @@
 /* eslint-disable */
 const path = require("path");
-const MomentLocalesPlugin = require(`moment-locales-webpack-plugin`);
 
 module.exports = {
   mode: "development",
   entry: "./src/main.js",
   output: {
     filename: "bundle.js",
-    path: path.join(__dirname, "public"),
+    path: path.join(__dirname, "public/js/"),
   },
-  devtool: "false",
+  devtool: "source-map",
   devServer: {
     contentBase: path.join(__dirname, "public"),
     watchContentBase: true,
   },
-  module: {
-    rules: [{
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader']
-    }]
-  },
-  plugins: [
-    new MomentLocalesPlugin()
-  ]
 };
